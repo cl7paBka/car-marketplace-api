9755d7df324367f50a1b2b7f500d3680017c3974
unknown <maks-kandikov@yandex.ru>
2025-01-15 23:51:09 +0300

    __init__ files

--- src/api/routes/orders.py
+++ src/api/routes/orders.py
@@ -38,34 +38,122 @@ async def create_order(
 ):
     return await service.create(order)
 
-# @router.get(
-#     path="/{order_id}"
-# )
 
-# @router.get(
-#     path="/status/{status}"
-# )
+@router.get(
+    path="/{order_id}",
+    response_model=BaseResponse[OrderSchema],
+    summary="Get order by ID",
+    description="""
+    
+    """,
+    # responses
+)
+async def get_order_by_id(
+        order_id: int,
+        service: Annotated[OrdersService, Depends(orders_service)]
+):
+    return await service.get_by_order_id(order_id)
 
-# @router.get(
-#     path="/customer_id/{customer_id}"
-# )
 
-# @router.get(
-#     path="/salesperson_id/{salesperson_id}"
-# )
+@router.get(
+    path="/status/{status}",
+    response_model=BaseResponse[List[OrderSchema]],
+    summary="Get orders by status",
+    description="""
+    
+    """,
+    # responses
+)
+async def get_orders_by_status(
+        status: OrderStatus,
+        service: Annotated[OrdersService, Depends(orders_service)]
+):
+    return await service.get_by_status(status)
 
-# @router.get(
-#     path="/car_id/{car_id}"
-# )
 
-# @router.get(
-#     path="/"
-# )
+@router.get(
+    path="/customer_id/{customer_id}",
+    response_model=BaseResponse[List[OrderSchema]],
+    summary="Get orders by customer's ID",
+    description="""
+    """,
+    # responses
+)
+async def get_orders_by_customer_id(
+        customer_id: int,
+        service: Annotated[OrdersService, Depends(orders_service)]
+):
+    return await service.get_by_customer_id(customer_id)
 
-# @router.patch(
-#     path="/patch/{order_id}"
-# )
 
-# @router.delete(
-#     path="/delete/{order_id}"
-# )
+@router.get(
+    path="/salesperson_id/{salesperson_id}",
+    response_model=BaseResponse[List[OrderSchema]],
+    summary="Get orders by salesperson's ID",
+    description="""
+    """
+    # responses
+)
+async def get_orders_by_salesperson_id(
+        salesperson_id: int,
+        service: Annotated[OrdersService, Depends(orders_service)]
+):
+    return await service.get_by_salesperson_id(salesperson_id)
+
+
+@router.get(
+    path="/car_id/{car_id}",
+    response_model=BaseResponse[List[OrderSchema]],
+    summary="Get orders by car's ID",
+    description="""
+    """,
+    # responses
+)
+async def get_orders_by_car_id(
+        car_id: int,
+        service: Annotated[OrdersService, Depends(orders_service)]
+):
+    return await service.get_by_car_id(car_id)
+
+
+@router.get(
+    path="/",
+    response_model=BaseResponse[List[OrderSchema]],
+    summary="Get all orders",
+    description="""
+    """
+    # responses
+)
+async def get_all_orders(service: Annotated[OrdersService, Depends(orders_service)]):
+    return await service.get_all()
+
+
+@router.patch(
+    path="/patch/{order_id}",
+    response_model=BaseResponse[OrderSchema],
+    summary="Update order details",
+    description="""
+    """
+    # responses
+)
+async def update_order_by_order_id(
+        order_id: int,
+        new_order: OrderUpdateSchema,
+        service: Annotated[OrdersService, Depends(orders_service)]
+):
+    return await service.update_by_id(order_id, new_order)
+
+
+@router.delete(
+    path="/delete/{order_id}",
+    response_model=BaseStatusMessageResponse,
+    summary="Delete an order",
+    description="""
+    """
+    # responses
+)
+async def delete_order_by_order_id(
+        order_id: int,
+        service: Annotated[OrdersService, Depends(orders_service)]
+):
+    return await service.delete_by_id(order_id)
