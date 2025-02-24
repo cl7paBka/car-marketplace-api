c1c68aebf1a2c14dccc794b2d471b0d9856179c8
cl7paBKa <maks-kandikov@yandex.ru>
2025-02-24 21:20:24 +0300

    Updated comments

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,6 @@ import os
 import asyncio
 import pytest
 import pytest_asyncio
-from dotenv import load_dotenv
 from httpx import AsyncClient, ASGITransport
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.exc import SQLAlchemyError
@@ -13,7 +12,6 @@ from src.db.db import Base, get_async_session
 from tests.utils.config import TEST_DB_USER, TEST_DB_PASSWORD, TEST_DB_HOST, TEST_DB_NAME, TEST_DB_PORT
 
 # --- Configuration ---
-load_dotenv()  # Load environment variables from .env
 DATABASE_URL = f"postgresql+asyncpg://{TEST_DB_USER}:{TEST_DB_PASSWORD}@{TEST_DB_HOST}:{TEST_DB_PORT}/{TEST_DB_NAME}"
 
 # Create the engine and the session factory for the test database
@@ -21,31 +19,31 @@ engine_test = create_async_engine(DATABASE_URL, future=True)
 TestSession = async_sessionmaker(bind=engine_test, expire_on_commit=False)
 
 
-# --- Event loop fixture --- (This is deprecated, so I commented it)
-# @pytest.fixture(scope="session")
-# def event_loop():
-#     """
-#     Creates one shared event loop for the entire test session.
-#
-#     Why do we need this?
-#     --------------------
-#     I'm making this pet-project on Windows 10, the default event loop policy for windows is ProactorEventLoop,
-#     which often conflicts with asyncpg when closing or performing network operations. This
-#     conflict can lead to errors like 'Event loop is closed' or
-#     "'NoneType' object has no attribute 'send'" because asyncpg might try to send
-#     or receive data after the loop is already shut down.
-#
-#     By switching to WindowsSelectorEventLoopPolicy and using a single event loop
-#     for all tests, I ensure that asyncpg can handle any remaining
-#     operations without having the event loop abruptly closed between tests.
-#     """
-#     if hasattr(asyncio, "WindowsSelectorEventLoopPolicy"):
-#         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-#
-#     loop = asyncio.new_event_loop()
-#     asyncio.set_event_loop(loop)
-#     yield loop
-#     loop.close()
+# --- Event loop fixture --- (This is deprecated, so I commented it and eventually switched from Windows 10 to Linux :D)
+@pytest.fixture(scope="session")
+def event_loop():
+    """
+    Creates one shared event loop for the entire test session.
+
+    Why do we need this?
+    --------------------
+    I'm making this pet-project on Windows 10, the default event loop policy for windows is ProactorEventLoop,
+    which often conflicts with asyncpg when closing or performing network operations. This
+    conflict can lead to errors like 'Event loop is closed' or
+    "'NoneType' object has no attribute 'send'" because asyncpg might try to send
+    or receive data after the loop is already shut down.
+
+    By switching to WindowsSelectorEventLoopPolicy and using a single event loop
+    for all tests, I ensure that asyncpg can handle any remaining
+    operations without having the event loop abruptly closed between tests.
+    """
+    if hasattr(asyncio, "WindowsSelectorEventLoopPolicy"):
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    yield loop
+    loop.close()
 
 
 # --- Database-related fixtures ---
